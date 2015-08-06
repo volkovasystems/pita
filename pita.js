abc3750306de46c570d9f392cd52e0a157cbe82f
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2015-08-06 11:33:51 +0800

    Fixed file traversal.

--- pita.js
+++ pita.js
@@ -3,22 +3,34 @@ var uglify = require( "gulp-uglify" );
 var rename = require( "gulp-rename" );
 var sourcemaps = require( "gulp-sourcemaps" );
 var clean = require( "gulp-clean" );
+var fs = require( "fs" );
+var path = require( "path" );
 
 var pita = function pita( script ){
-    gulp.task( "clean", function cleanTask( ){
-        return gulp.src( "./@script.min.js".replace( "@script", script ) )
-            .pipe( clean( { "force": true } ) );
-    } );
+	var scriptPath = "@script.js".replace( "@script", script );
+	var minifiedScriptPath = "@script.min.js".replace( "@script", script );
 
-    gulp.task( "default",
-        [ "clean" ],
-        function defaultTask( ){
-            return gulp.src( "./@script.js".replace( "@script", script ) )
-                .pipe( uglify( ) )
-                .pipe( rename( "./@script.min.js".replace( "@script", script ) ) )
-                .pipe( sourcemaps.write( "./" ) )
-                .pipe( gulp.dest( "./" ) );
-        } );
+	var pathSteps = "./";
+	while( !fs.existsSync( path.resolve( pathSteps, scriptPath ) ) ){
+		pathSteps = "../" + pathSteps;
+	}
+
+	gulp.task( "clean", function cleanTask( ){
+		console.log( "cleaning previous script version ", path.resolve( pathSteps, minifiedScriptPath ) );
+		return gulp.src( path.resolve( pathSteps, minifiedScriptPath ) )
+			.pipe( clean( { "force": true } ) );
+	} );
+
+	gulp.task( "default",
+		[ "clean" ],
+		function defaultTask( ){
+			console.log( "flattening script ", path.resolve( pathSteps, scriptPath ) );
+			return gulp.src( path.resolve( pathSteps, scriptPath ) )
+				.pipe( uglify( ) )
+				.pipe( rename( path.resolve( pathSteps, minifiedScriptPath ) ) )
+				.pipe( sourcemaps.write( pathSteps ) )
+				.pipe( gulp.dest( pathSteps ) );
+		} );
 };
 
 module.exports = pita;
